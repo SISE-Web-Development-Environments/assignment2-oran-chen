fb160c9d0f4e71a73d5c2858d4584907f9441ef1
Oran <shichman@post.bgu.ac.il>
2020-05-01 10:34:22 +0300

    -

--- settings.js
+++ settings.js
@@ -75,3 +75,31 @@ $().ready(function () {
         }
     });
 });
+
+function randomInputs() {
+
+    //Change number of balls
+    var randomNumber = Math.random()*100;
+    while (randomNumber < 50 || randomNumber > 90){
+        randomNumber = Math.random()*100;
+    }
+    randomNumber = Math.floor(randomNumber);
+    numOfBalls = randomNumber;
+    document.getElementById('ballsnumin'). value = randomNumber;
+
+    //Change colors
+    document.getElementById('sixtyColor').value = "#"+((1<<24)*Math.random()|0).toString(16);
+    document.getElementById('thirtyColor').value = "#"+((1<<24)*Math.random()|0).toString(16);
+    document.getElementById('tenColor').value = "#"+((1<<24)*Math.random()|0).toString(16);
+
+    //Change ghosts number
+    document.getElementById('ghostsNumber').selectedIndex = Math.floor(Math.random() * 4);
+
+    //Change time number
+    randomNumber = Math.random()*100;
+    while (randomNumber < 60){
+        randomNumber = Math.random()*100;
+    }
+    randomNumber = Math.floor(randomNumber);
+    document.getElementById('timenumin'). value = randomNumber;
+}
